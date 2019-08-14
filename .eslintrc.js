619dac4ac9228bc531ac178915d1642c74a8c482
Dmitry Kryukov <remasik@gmail.com>
2019-08-14 07:45:39 -0700

    Initial structure of quasar app

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,57 @@
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
+
   parserOptions: {
+    parser: 'babel-eslint',
     sourceType: 'module'
   },
+
   env: {
-    browser: true,
-    node: true
+    browser: true
   },
-  extends: 'standard',
-  globals: {
-    __static: true
-  },
-  plugins: [
-    'html'
+
+  extends: [
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    'plugin:vue/essential',
+    '@vue/standard'
   ],
-  'rules': {
-    // allow paren-less arrow functions
-    'arrow-parens': 0,
+
+  // required to lint *.vue files
+  plugins: [
+    'vue'
+  ],
+
+  globals: {
+    'ga': true, // Google Analytics
+    'cordova': true,
+    '__statics': true,
+    'process': true
+  },
+
+  // add your custom rules here
+  rules: {
     // allow async-await
-    'generator-star-spacing': 0,
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    'no-unused-vars': 0
+    'generator-star-spacing': 'off',
+    // allow paren-less arrow functions
+    'arrow-parens': 'off',
+    'one-var': 'off',
+
+    'no-unused-vars': 'off',
+
+    'import/first': 'off',
+    'import/named': 'error',
+    'import/namespace': 'error',
+    'import/default': 'error',
+    'import/export': 'error',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'prefer-promise-reject-errors': 'off',
+
+    // allow console.log during development only
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // allow debugger during development only
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
 }
