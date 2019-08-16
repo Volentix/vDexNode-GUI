2de63e1fa1beba0c835c700a6649f98743f4c7a5
Dmitry Kryukov <remasik@gmail.com>
2019-08-16 12:02:55 -0700

    Added functionality to get account for a key and to get a public key from a private

--- src/util/EosWrapper.js
+++ src/util/EosWrapper.js
@@ -1,5 +1,6 @@
 import { Api, JsonRpc } from 'eosjs'
 import { JsSignatureProvider } from 'eosjs/dist/eosjs-jssig'
+import ecc from 'eosjs-ecc'
 const { TextEncoder, TextDecoder } = require('util')
 const fetch = require('node-fetch')
 
@@ -7,8 +8,20 @@ class EosWrapper {
   constructor (keyProvider) {
     this.rpc = new JsonRpc(process.env.EOS_ENDPOINT, { fetch })
     const rpc = this.rpc
-    const signatureProvider = new JsSignatureProvider([keyProvider])
-    this.api = new Api({ rpc, signatureProvider, textDecoder: new TextDecoder(), textEncoder: new TextEncoder() })
+    if (arguments.length) {
+      const signatureProvider = new JsSignatureProvider([keyProvider])
+      this.api = new Api({ rpc, signatureProvider, textDecoder: new TextDecoder(), textEncoder: new TextEncoder() })
+    }
+  }
+
+  privateToPublic (wif) {
+    let pubKey = ecc.privateToPublic(wif)
+    return pubKey
+  }
+
+  async getAccounts (wif) {
+    let accounts = await this.rpc.history_get_key_accounts(wif)
+    return accounts
   }
 }
 
