640eb2ed946893b3f27de95b47bcd4495955c84b
Dmitry Kryukov <remasik@gmail.com>
2019-08-30 11:11:58 -0700

    Add utils file with additional, common functions

---
+++ src/util/utils.js
@@ -0,0 +1,12 @@
+function getUnique (arr, comp) {
+  const unique = arr.map(e => e[comp])
+    // store the keys of the unique objects
+    .map((e, i, final) => final.indexOf(e) === i && i)
+    // eliminate the dead keys & store unique objects
+    .filter(e => arr[e]).map(e => arr[e])
+  return unique
+}
+
+export {
+  getUnique
+}
