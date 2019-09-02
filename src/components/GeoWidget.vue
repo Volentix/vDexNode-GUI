4c415f471b2779c7257909895250d63d04203f21
Dmitry Kryukov <remasik@gmail.com>
2019-09-02 10:14:13 -0700

    Some fixes in the map widget. Add reactive reading of locations instead of a file

--- src/components/GeoWidget.vue
+++ src/components/GeoWidget.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="bg-blue-grey-10 inset-shadow">
-    <div id="map"></div>
-    <div id="tooltip" class="hidden">
-      <p>Name: <span id="name">Name</span></p>
-      <p>Year: <span id="year">Year</span></p>
-      <p>Class: <span id="class">Class</span></p>
-      <p>Mass: <span id="mass">Mass</span></p>
+  <div class='bg-blue-grey-10 inset-shadow'>
+    <div id='map'></div>
+    <div id='tooltip' class='hidden bg-blue-grey-6'>
+      <p>Name: <span id='name'>Name</span></p>
+      <p>Year: <span id='year'>Year</span></p>
+      <p>Class: <span id='class'>Class</span></p>
+      <p>Mass: <span id='mass'>Mass</span></p>
     </div>
   </div>
 </template>
@@ -17,11 +17,12 @@ const topojson = require('topojson')
 
 export default {
   name: 'GeoWidget',
-  props: {
-    geoData: String
-  },
+  props: ['geoData'],
   data () {
     return {
+      mapPath: '../assets/countries.geo.json' || 'https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json',
+      locPath: '/initial_node_list.json',
+      nodeGeoData: {}
     }
   },
   mounted () {
@@ -29,10 +30,41 @@ export default {
   },
   methods: {
     load () {
-      d3.json('https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json', function (json) {
-        var widgetWidth = Math.round(window.getComputedStyle(document.getElementById('map')).width.split('px')[0])
-        // width and height
-        var w = widgetWidth
+      this.getNodeGeoData()
+      this.mapLoad()
+    },
+    getNodeGeoData () {
+      // Temporary solution, get the initial list of node locations from the file
+      const fs = require('fs')
+      const path = require('path')
+      const jsonString = fs.readFileSync(path.join(__dirname, '../assets') + this.locPath, 'utf8')
+      this.nodeGeoData = JSON.parse(jsonString)
+
+      // TODO: Get the list of locations through API
+      this.nodeGeoData.features.push({
+        /* eslint-disable */
+        "properties": {
+          "mass": "200000",
+          "name": "Moscow",
+          "reclong": "37.618423",
+          "geolocation_address": null,
+          "geolocation_zip": null,
+          "year": "1880-01-01T00:00:00.000",
+          "geolocation_state": null,
+          "fall": "Fell",
+          "id": "1",
+          "recclass": "L5",
+          "reclat": "55.751244",
+          "geolocation_city": null,
+          "nametype": "Valid"
+        }
+      })
+      /* eslint-enable */
+    },
+    mapLoad () {
+      var self = this
+      d3.json(this.mapPath, function (json) {
+        var w = Math.round(window.getComputedStyle(document.getElementById('map')).width.split('px')[0])
         var h = 400
 
         // container svg
@@ -57,211 +89,80 @@ export default {
           .attr('d', land)
           .style('fill', '#171717')
 
-        // ASTEROIDS
-        var meteors
-        d3.json('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/meteorite-strike-data.json', function (data) {
-          meteors = svg.selectAll('circle')
-            .data(data.features)
-            .enter()
-            .append('circle')
-            .attr('cx', function (d) {
-              return projection([d.properties.reclong, d.properties.reclat])[0]
-            })
-            .attr('cy', function (d) {
-              return projection([d.properties.reclong, d.properties.reclat])[1]
-            })
-            .attr('r', function (d) {
-              if (Math.floor(d.properties.mass) > 2000000) {
-                return 20
-              } else if (Math.floor(d.properties.mass) > 1000000) {
-                return 10
-              } else if (Math.floor(d.properties.mass) > 100000) {
-                return 5
-              } else {
-                return 2
-              }
-            })
-            .style('fill', '#e91e63')
-            .style('opacity', 0.75)
-
-          // TOOLTIP
-          meteors.on('mouseover', function (d) {
-            var xPosition = parseFloat(d3.select(this).attr('cx')) + 20 + widgetWidth
-            if (xPosition > window.innerWidth / 2 + 30) {
-              xPosition = parseFloat(d3.select(this).attr('cx')) - 220 + widgetWidth
+        // node locations
+        var nodes
+        var data = self.nodeGeoData
+        nodes = svg.selectAll('circle')
+          .data(data.features)
+          .enter()
+          .append('circle')
+          .attr('cx', function (d) {
+            return projection([d.properties.reclong, d.properties.reclat])[0]
+          })
+          .attr('cy', function (d) {
+            return projection([d.properties.reclong, d.properties.reclat])[1]
+          })
+          .attr('r', function (d) {
+            if (Math.floor(d.properties.mass) > 2000000) {
+              return 20
+            } else if (Math.floor(d.properties.mass) > 1000000) {
+              return 10
+            } else if (Math.floor(d.properties.mass) > 100000) {
+              return 5
+            } else {
+              return 2
             }
-            var yPosition = parseFloat(d3.select(this).attr('cy')) + 50
-            // Update the tooltip position and value
-            d3.select('#tooltip')
-              .style('left', xPosition + 'px')
-              .style('top', yPosition + 'px')
-              .select('#name')
-              .text(d.properties.name)
-            d3.select('#tooltip')
-              .select('#class')
-              .text(d.properties.recclass)
-            d3.select('#tooltip')
-              .select('#mass')
-              .text(d.properties.mass)
-            d3.select('#tooltip')
-              .select('#year')
-              .text(d.properties.year.slice(0, 4))
-            // Show the tooltip
-            d3.select('#tooltip').classed('hidden', false)
           })
+          .style('fill', '#e91e63')
+          .style('opacity', 0.75)
 
-          meteors.on('mouseout', function () {
-            // Hide the tooltip
-            d3.select('#tooltip').classed('hidden', true)
-          })
+        // TOOLTIP
+        nodes.on('mouseover', function (d) {
+          var xPosition = parseFloat(d3.select(this).attr('cx')) + 20 + w
+          if (xPosition > w / 2 + 30) {
+            xPosition = parseFloat(d3.select(this).attr('cx')) - 220 + w
+          }
+          var yPosition = parseFloat(d3.select(this).attr('cy')) + 50
+          // Update the tooltip position and value
+          d3.select('#tooltip')
+            .style('left', xPosition + 'px')
+            .style('top', yPosition + 'px')
+            .select('#name')
+            .text(d.properties.name)
+          d3.select('#tooltip')
+            .select('#class')
+            .text(d.properties.recclass)
+          d3.select('#tooltip')
+            .select('#mass')
+            .text(d.properties.mass)
+          d3.select('#tooltip')
+            .select('#year')
+            .text(d.properties.year.slice(0, 4))
+          // Show the tooltip
+          d3.select('#tooltip').classed('hidden', false)
+        })
+
+        nodes.on('mouseout', function () {
+          // Hide the tooltip
+          d3.select('#tooltip').classed('hidden', true)
         })
 
         // RESPONSIVE WIDTH
         window.onresize = function () {
           // new width
           // var width = window.innerWidth / 2
-          var width = widgetWidth / 2
+          var width = w / 2
           svg.attr('width', width)
           // new map
           var newProjection = d3.geo.mercator().translate([width / 2, h / 2])
           land.projection(newProjection)
           world.attr('d', land)
-          meteors.attr('cx', function (d) {
+          nodes.attr('cx', function (d) {
             return newProjection([d.properties.reclong, d.properties.reclat])[0]
           })
         }
       })
-    }, // end of load
-    load2 () {
-      // Set svg width & height
-      let centered
-      const mapCenter = {
-        lat: 1.4,
-        lng: 117.5
-      }
-      const size = {
-        height: 700,
-        width: d3.select('.map-wrapper').node().getBoundingClientRect().width
-      }
-
-      const color = d3.scale.linear()
-        .domain([1, 20])
-        .clamp(true)
-        .range(['#08304b', '#08304b'])
-
-      const projection = d3.geo.equirectangular()
-        .scale(1400)
-        .center([mapCenter.lng, mapCenter.lat])
-        .translate([size.width / 2, size.height / 2])
-
-      const path = d3.geo.path()
-        .projection(projection)
-
-      const svg = d3.select('svg')
-        .attr('width', size.width)
-        .attr('height', size.height)
-
-      // Add background
-      svg.append('rect')
-        .attr('class', 'background')
-        .attr('width', size.width)
-        .attr('height', size.height)
-        // .on('click', clicked)
-        .on('click', console.log('clicked'))
-
-      const g = svg.append('g')
-
-      const effectLayer = g.append('g')
-        .classed('effect-layer', true)
-      const mapLayer = g.append('g')
-        .classed('map-layer', true)
-
-      // Load map data
-      const geoJsonUrl = 'https://raw.githubusercontent.com/superpikar/indonesia-geojson/master/indonesia.geojson'
-      d3.json(geoJsonUrl, function (error, mapData) {
-        if (error) throw error
-        var features = mapData.features
-
-        // Update color scale domain based on data
-        color.domain([0, d3.max(features, nameLength)])
-
-        // Draw each province as a path
-        mapLayer.selectAll('path')
-          .data(features)
-          .enter().append('path')
-          .attr('d', path)
-          .attr('vector-effect', 'non-scaling-stroke')
-          .style('fill', fillFn)
-          // .on('mouseover', mouseover)
-          // .on('mouseout', mouseout)
-          // .on('click', clicked)
-          .on('click', console.log('clicked'))
-      })
-
-      // function clicked(d) {
-      //   var x, y, k;
-
-      //   // Compute centroid of the selected path
-      //   if (d && centered !== d) {
-      //     var centroid = path.centroid(d);
-      //     x = centroid[0];
-      //     y = centroid[1];
-      //     k = 4;
-      //     centered = d;
-      //     app.openInfo(d.properties);
-      //   } else {
-      //     x = size.width / 2;
-      //     y = size.height / 2;
-      //     k = 1;
-      //     centered = null;
-      //     app.closeInfo();
-      //   }
-
-      //   // Highlight the clicked province
-      //   mapLayer.selectAll('path')
-      //     .style('fill', function(d){
-      //       return centered && d===centered ? '#D5708B' : fillFn(d);
-      //   });
-
-      //   // Zoom
-      //   g.transition()
-      //     .duration(750)
-      //     .attr('transform', 'translate(' + size.width / 2 + ',' + size.height / 2 + ')scale(' + k + ')translate(' + -x + ',' + -y + ')');
-      // }
-
-      // function mouseover(d){
-      //   // Highlight hovered province
-      //   d3.select(this).style('fill', '#1483ce');
-      //   if(d) {
-      //     app.selectProvince(d.properties);
-      //   }
-      // }
-
-      // function mouseout(d){
-      //   app.selectProvince(undefined);
-      //   // Reset province color
-      //   mapLayer.selectAll('path')
-      //     .style('fill', (d) => {
-      //       return centered && d===centered ? '#D5708B' : fillFn(d);
-      //     });
-      // }
-
-      // Get province name length
-      function nameLength (d) {
-        const n = nameFn(d)
-        return n ? n.length : 0
-      }
-
-      // Get province name
-      function nameFn (d) {
-        return d && d.properties ? d.properties.name : null
-      }
-
-      // Get province color
-      function fillFn (d) {
-        return color(nameLength(d))
-      }
-    } // end of load2
+    }
   } // end of methods
 }
 </script>
@@ -276,7 +177,6 @@ svg.chart-wrap {
   width: 200px;
   height: auto;
   padding: 10px;
-  background-color: white;
   color: #000000;
   -webkit-box-shadow: 4px 4px 10px rgba(0, 0, 0, 0.4);
   -moz-box-shadow: 4px 4px 10px rgba(0, 0, 0, 0.4);
