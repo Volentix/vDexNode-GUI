61e45f6d540308c54af2f09ed6ac0c26429ea1dc
Dmitry Kryukov <remasik@gmail.com>
2019-09-27 09:46:52 -0700

    init store

--- src/store/index.js
+++ src/store/index.js
@@ -1,25 +1,54 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+// import VuexPersist from 'vuex-persist'
 
-// import example from './module-example'
+// const vuexPersist = new VuexPersist({
+//   key: 'vdex',
+//   storage: window.localStorage
+// })
 
 Vue.use(Vuex)
 
-/*
- * If not building with SSR mode, you can
- * directly export the Store instantiation
- */
-
-export default function (/* { ssrContext } */) {
-  const Store = new Vuex.Store({
-    modules: {
-      // example
-    },
-
-    // enable strict mode (adds overhead!)
-    // for dev mode only
-    strict: process.env.DEV
-  })
-
-  return Store
+function initialState () {
+  return {
+    loggedIn: false
+  }
 }
+
+const store = new Vuex.Store({
+  // plugins: [vuexPersist.plugin],
+  state: initialState,
+  getters: {
+    getLoggedIn: state => {
+      return state.loggedIn
+    }
+  },
+  mutations: {
+    resetState: (state) => {
+      Object.assign(state, initialState())
+    },
+    setLoggedIn: state => {
+      state.loggedIn = true
+    }
+  },
+  actions: {
+    // logout: (context) => {
+    //   return new Promise((resolve, reject) => {
+    //     context.commit('resetState')
+    //     resolve()
+    //   })
+    // },
+    // login: ({ context }, privateKey, publicKey, accountName) => {
+    //   console.log(privateKey, publicKey, accountName)
+    //   return new Promise((resolve, reject) => {
+    //     context.commit('setLoggedIn')
+    //     context.commit('setPrivateKey', privateKey)
+    //     context.commit('setPublicKey', publicKey)
+    //     context.commit('setAccountName', accountName)
+    //     resolve()
+    //   })
+    // }
+  }
+})
+
+export default store
