959ecc67c0ddfb43b51d7f0c607723f212617d2f
Dmitry Kryukov <remasik@gmail.com>
2019-09-30 10:27:37 -0700

    Add new result handler. Will show Notify widget with short messages instead of dialog

---
+++ src/util/resultHandler.js
@@ -0,0 +1,33 @@
+import { Notify } from 'quasar'
+
+class ResultHandler {
+  constructor (message, stack) {
+    Notify.setDefaults({
+      position: 'top',
+      textColor: 'vdark',
+      timeout: 60000,
+      color: 'vgreen',
+      icon: 'fas fa-check-circle',
+      multiLine: true,
+      html: true,
+      actions: [
+        { label: 'Got it', color: 'vdark', handler: () => { /* console.log('wooow') */ } }
+      ]
+    })
+    const details = JSON.stringify(stack, null, 2)
+    return { message, details }
+  }
+}
+
+export const userResult = (message, stack) => {
+  if (!process.env.PROD) return devResult(message, stack)
+  const result = new ResultHandler(message, stack)
+  Notify.create({ message: result.message })
+}
+
+export const devResult = (message, stack) => {
+  if (process.env.PROD) return
+  const result = new ResultHandler(message, stack)
+  Notify.create({ message: result.message })
+  // Notify.create({ message: result.message + '<p>' + result.details + '</p>' })
+}
