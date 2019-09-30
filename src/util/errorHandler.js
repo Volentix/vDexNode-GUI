7664675b5ef3c80bd81bd3fca365a8a97496b440
Dmitry Kryukov <remasik@gmail.com>
2019-09-30 12:17:15 -0700

    Add new error handler.

---
+++ src/util/errorHandler.js
@@ -0,0 +1,34 @@
+import { Notify } from 'quasar'
+
+class ErrorHandler extends Error {
+  constructor (err, point) {
+    Notify.setDefaults({
+      position: 'top-right',
+      textColor: 'vgrey',
+      timeout: 60000,
+      color: 'negative',
+      icon: 'report_problem',
+      multiLine: true,
+      html: true,
+      actions: [
+        { label: 'Got it', color: 'vgrey', handler: () => { /* console.log('wooow') */ } }
+      ]
+    })
+    const error = super(err)
+    error.point = point
+    return error
+  }
+}
+
+export const userError = (err, point) => {
+  if (!process.env.PROD) return devError(err, point)
+  const error = new ErrorHandler(err, point)
+  Notify.create({ message: error.point + '<br>' + error.message })
+}
+
+export const devError = (err, point) => {
+  if (process.env.PROD) return
+  const error = new ErrorHandler(err, point)
+  Notify.create({ message: error.point + '<br>' + error.message })
+  // throw error
+}
