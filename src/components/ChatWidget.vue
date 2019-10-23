eedb53f4ae9e9a0759ee40254943c1eff3927b6a
Dmitry Kryukov <remasik@gmail.com>
2019-10-23 08:48:58 -0700

    Move chat into separate component

---
+++ src/components/ChatWidget.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="row">
+    <div class="col-4 text-vgrey bg-vdark">
+      <q-banner dense class="text-vgrey bg-vdark q-px-md q-pt-md">
+        <div class="text-uppercase">Chat</div>
+      </q-banner>
+      <q-separator dark />
+      <q-list>
+        <q-item clickable>
+          <q-item-section>
+            <q-item-label>Item 1</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable>
+          <q-item-section>
+            <q-item-label>Item 2</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable>
+          <q-item-section>
+            <q-item-label>Item 3</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </div>
+    <div class="col-8 bg-vdarker">
+      <div class="column">
+        <div class="col scroll q-px-md text-vgrey" style="min-height: 30vh; max-height: 30vh">
+          <q-chat-message name="me" bg-color="vgreen" :text="['hey, how are you?']" sent />
+          <q-chat-message name="Support" bg-color="vdark" text-color="white" :text="['doing fine, how r you?']" />
+          <q-chat-message name="me" bg-color="vgreen" :text="['hey, how are you?']" sent />
+          <q-chat-message name="Support" bg-color="vdark" text-color="white" :text="['doing fine, how r you?']" />
+          <q-chat-message name="me" bg-color="vgreen" :text="['hey, how are you?']" sent />
+          <q-chat-message name="Support" bg-color="vdark" text-color="white" :text="['doing fine, how r you?']" />
+          <q-chat-message name="me" bg-color="vgreen" :text="['hey, how are you?']" sent />
+          <q-chat-message name="Support" bg-color="vdark" text-color="white" :text="['doing fine, how r you?']" />
+        </div>
+        <div class="col q-px-md q-my-md">
+          <q-input dense rounded outlined dark color="vgreen">
+            <template v-slot:after>
+              <q-btn round dense flat icon="send" />
+            </template>
+          </q-input>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ChatWidget',
+  data () {
+    return {}
+  },
+  mounted () {},
+  watch: {},
+  methods: {} // end of methods
+}
+</script>
+
+<style scoped>
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -378,52 +378,7 @@
           <!-- Chat -->
           <div class="col-5">
             <q-banner dense inline-actions class="text-vdark bg-vpurple">The chat widget is currently disabled, the data is artificial.</q-banner>
-            <div class="row">
-              <div class="col-4 text-vgrey bg-vdark">
-                <q-banner dense class="text-vgrey bg-vdark q-px-md q-pt-md">
-                  <div class="text-uppercase">Chat</div>
-                </q-banner>
-                <q-separator dark />
-                <q-list>
-                  <q-item clickable>
-                    <q-item-section>
-                      <q-item-label>Item 1</q-item-label>
-                    </q-item-section>
-                  </q-item>
-                  <q-item clickable>
-                    <q-item-section>
-                      <q-item-label>Item 2</q-item-label>
-                    </q-item-section>
-                  </q-item>
-                  <q-item clickable>
-                    <q-item-section>
-                      <q-item-label>Item 3</q-item-label>
-                    </q-item-section>
-                  </q-item>
-                </q-list>
-              </div>
-              <div class="col-8 bg-vdarker">
-                <div class="column">
-                  <div class="col scroll q-px-md text-vgrey" style="min-height: 30vh; max-height: 30vh">
-                    <q-chat-message name="me" bg-color="vgreen" :text="['hey, how are you?']" sent />
-                    <q-chat-message name="Support" bg-color="vdark" text-color="white" :text="['doing fine, how r you?']" />
-                    <q-chat-message name="me" bg-color="vgreen" :text="['hey, how are you?']" sent />
-                    <q-chat-message name="Support" bg-color="vdark" text-color="white" :text="['doing fine, how r you?']" />
-                    <q-chat-message name="me" bg-color="vgreen" :text="['hey, how are you?']" sent />
-                    <q-chat-message name="Support" bg-color="vdark" text-color="white" :text="['doing fine, how r you?']" />
-                    <q-chat-message name="me" bg-color="vgreen" :text="['hey, how are you?']" sent />
-                    <q-chat-message name="Support" bg-color="vdark" text-color="white" :text="['doing fine, how r you?']" />
-                  </div>
-                  <div class="col q-px-md q-my-md">
-                    <q-input dense rounded outlined dark color="vgreen">
-                      <template v-slot:after>
-                        <q-btn round dense flat icon="send" />
-                      </template>
-                    </q-input>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <ChatWidget />
           </div>
         </div>
 
@@ -569,6 +524,7 @@
 
 <script>
 import MapWidget from '../components/MapWidget.vue'
+import ChatWidget from '../components/ChatWidget.vue'
 
 /**
  * Main page. Loads other components
@@ -600,7 +556,8 @@ import MapWidget from '../components/MapWidget.vue'
 export default {
   name: 'index',
   components: {
-    MapWidget
+    MapWidget,
+    ChatWidget
   },
   data () {
     return {
