03038eab89542aee1206372613c25bc4c671e85f
Dmitry Kryukov <remasik@gmail.com>
2019-10-23 09:39:41 -0700

    [WIP] New error page.

--- src/pages/Error404.vue
+++ src/pages/Error404.vue
@@ -1,14 +1,17 @@
 <template>
-  <div class="fixed-center text-center">
-    <p>
-      <img src="~assets/sad.svg" style="width:30vw;max-width:150px;" />
-    </p>
-    <p class="text-faded">
-      Sorry, nothing here...
-      <strong>(404)</strong>
-    </p>
-    <q-btn color="secondary" style="width:200px;" @click="$router.push('/')">Go back</q-btn>
-  </div>
+  <q-layout>
+    <div class="titlebar"></div>
+    <q-page-container>
+      <q-page class="bg-vblack q-pa-lg">
+        <div class="row text-center items-center q-pa-xl">
+          <div class="col">
+            <div class="text-vgrey text-uppercase text-h1 q-pa-xl">Not found</div>
+            <q-btn outline rounded color="vgreen" class="q-mx-xs" label="Go Home" to="/" />
+          </div>
+        </div>
+      </q-page>
+    </q-page-container>
+  </q-layout>
 </template>
 
 <script>
